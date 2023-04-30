0cdd7743e3810aa7c7921867efe49530b06b3970
jmeegan2 <jmeegan8@gmail.com>
2023-04-29 21:04:47 -0400

    image is clickable, need navigation

--- app/src/main/java/edu/towson/cosc435/meegan/assignment5_redo/MainActivity.kt
+++ app/src/main/java/edu/towson/cosc435/meegan/assignment5_redo/MainActivity.kt
@@ -9,22 +9,27 @@ import androidx.compose.material.Text
 import androidx.compose.ui.tooling.preview.Preview
 import edu.towson.cosc435.meegan.assignment5_redo.ui.theme.Assignment5_redoTheme
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.runtime.Composable
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
-import coil.compose.rememberAsyncImagePainter
+import coil.compose.*
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Assignment5_redoTheme {
-               ImageList()
+               ImageGrid()
             }
         }
     }
@@ -54,18 +59,64 @@ fun SingleImage() {
     )
 }
 
+@Composable //for testing of circular progress indicator
+fun DelayedImage(
+    url: String,
+    delayMillis: Long,
+    content: @Composable (String) -> Unit
+) {
+    var imageUrl by remember { mutableStateOf<String?>(null) }
+
+    LaunchedEffect(url) {
+        delay(delayMillis)
+        imageUrl = url
+    }
+
+    if (imageUrl != null) {
+        content(imageUrl!!)
+    }
+}
+
+
 @Composable
-fun ImageList() {
+fun ImageGrid() {
     LazyVerticalGrid(
         columns = GridCells.Adaptive(minSize = 200.dp)
-    ){
+    ) {
         items(20) { index ->
             val imageUrl = "https://picsum.photos/200/200?random=$index"
-            val painter: Painter = rememberAsyncImagePainter(imageUrl)
-            Image(
-                painter = painter,
-                contentDescription = null,
+
+            Box(
                 modifier = Modifier.aspectRatio(1f)
+                    .clickable { }
+            ) {
+                DelayedImage(url = imageUrl, delayMillis = 100) { delayedImageUrl ->
+                    SubcomposeAsyncImage(
+                        model = delayedImageUrl,
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.matchParentSize()
+                    ) {
+                        val state = painter.state
+                        if (state is AsyncImagePainter.State.Loading || state is AsyncImagePainter.State.Error) {
+                            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+                        } else {
+                            SubcomposeAsyncImageContent()
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun ImageDetailScreen(imageUrl: String) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            Image(
+                painter = rememberAsyncImagePainter(model = imageUrl.replace("200", "800")),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.matchParentSize()
             )
         }
     }
