8ded57bdbd4b3f92c4f796c452e1b940d3074165
jmeegan2 <jmeegan8@gmail.com>
2023-04-30 14:32:09 -0400

    kinda working, randomness not working though

--- app/src/main/java/edu/towson/cosc435/meegan/assignment5_redo/MainActivity.kt
+++ app/src/main/java/edu/towson/cosc435/meegan/assignment5_redo/MainActivity.kt
@@ -1,62 +1,60 @@
 package edu.towson.cosc435.meegan.assignment5_redo
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.ui.tooling.preview.Preview
-import edu.towson.cosc435.meegan.assignment5_redo.ui.theme.Assignment5_redoTheme
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.*
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import coil.compose.AsyncImagePainter
+import coil.compose.SubcomposeAsyncImage
+import coil.compose.SubcomposeAsyncImageContent
+import coil.compose.rememberAsyncImagePainter
+import edu.towson.cosc435.meegan.assignment5_redo.ui.theme.Assignment5_redoTheme
 import kotlinx.coroutines.delay
+import android.util.Log
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val navController = rememberNavController()
             Assignment5_redoTheme {
-               ImageGrid()
+                NavHost(navController, startDestination = "imageGrid") {
+                    composable("imageGrid") { ImageGrid(navController) }
+                    composable("imageDetail/{imageUrl}") { backStackEntry ->
+                        val imageUrl = backStackEntry.arguments?.getString("imageUrl")!!
+                        ImageDetailScreen(imageUrl)
+                    }
+                }
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     Assignment5_redoTheme {
-        Greeting("Android")
-    }
-}
 
-@Composable
-fun SingleImage() {
-    val imageUrl = "https://i.ibb.co/njkHJHs/70438902852-4-B295341-8-AAA-4678-AC5-E-2-C7-B56-C80-FF4.jpg"
-    val painter: Painter = rememberAsyncImagePainter(imageUrl)
-    Image(
-        painter = painter,
-        contentDescription = null,
-        modifier = Modifier.fillMaxSize()
-    )
+    }
 }
 
 @Composable //for testing of circular progress indicator
@@ -77,22 +75,29 @@ fun DelayedImage(
     }
 }
 
+val imageIds = listOf(
+   0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21
+)
 
 @Composable
-fun ImageGrid() {
+fun ImageGrid(navController: NavController) {
     LazyVerticalGrid(
         columns = GridCells.Adaptive(minSize = 200.dp)
     ) {
-        items(20) { index ->
-            val imageUrl = "https://picsum.photos/200/200?random=$index"
+        items(imageIds.size) { index ->
+            val imageUrl = "https://picsum.photos/id/${imageIds[index]}/200/200"
 
             Box(
                 modifier = Modifier.aspectRatio(1f)
-                    .clickable { }
+                    .clickable {
+                        navController.navigate("imageDetail/${Uri.encode(imageUrl)}")
+                        Log.d("ImageGrid", "https://picsum.photos/id/${imageIds[index]}/200/200")
+                        Log.d("ImageGrid", "imageUrl value: $imageUrl")
+                    }
             ) {
                 DelayedImage(url = imageUrl, delayMillis = 100) { delayedImageUrl ->
                     SubcomposeAsyncImage(
-                        model = delayedImageUrl,
+                        model = delayedImageUrl!!,
                         contentDescription = null,
                         contentScale = ContentScale.Crop,
                         modifier = Modifier.matchParentSize()
@@ -108,16 +113,19 @@ fun ImageGrid() {
             }
         }
     }
+}
 
-    @Composable
-    fun ImageDetailScreen(imageUrl: String) {
-        Box(modifier = Modifier.fillMaxSize()) {
-            Image(
-                painter = rememberAsyncImagePainter(model = imageUrl.replace("200", "800")),
-                contentDescription = null,
-                contentScale = ContentScale.Crop,
-                modifier = Modifier.matchParentSize()
-            )
-        }
+@Composable
+fun ImageDetailScreen(imageUrl: String) {
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        Log.d("ImageDetailScreen", "imageUrl value: $imageUrl")
+        Image(
+
+            painter = rememberAsyncImagePainter(model = imageUrl.replace("200", "800")),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.matchParentSize()
+        )
     }
 }
